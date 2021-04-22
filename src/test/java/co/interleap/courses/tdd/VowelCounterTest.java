2d0996d88afa86305dc4f8909da5925a07bc6099
ginni-nand <ginni1671.cse18@chitkara.edu.in>
2021-04-22 11:35:43 +0530

    updates

--- src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
+++ src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
@@ -5,5 +5,16 @@ import org.junit.Test;
 public class VowelCounterTest {
 
     @Test
-    public void dummy(){}
+    public void shouldCountZeroWithAnEmptyString(){
+            assertEquals(0, new VowelCounter().count(""));
+    }
+    @Test
+    public void shouldCountSingleVowel()
+    {
+        assertEquals(1,new VowelCounter().count("u"));
+    }
+    public void shouldCountAllVowelsInStringHavingVowels()
+    {
+        assertEquals(5,new VowelCounter().count("aeiou"));
+    }
 }
